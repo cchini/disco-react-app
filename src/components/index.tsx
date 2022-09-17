92b5819007d1fd94c104746f5557cdbeba95a07d
camila-frangp <camila.frangp@gmail.com>
2022-09-16 23:32:06 -0300

    new componente checkbox

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,6 @@ import Select from './Select/Select';
 import Input from './Input/Input';
 import Modal from './Modal/Modal';
 import Switch from './Switch/Switch';
+import Checkbox from './Checkbox/Checkbox';
 
-export { Button, Select, Input, Modal, Switch };
+export { Button, Select, Input, Modal, Switch, Checkbox };
