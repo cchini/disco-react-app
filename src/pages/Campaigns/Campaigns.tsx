dcdb07b51067f1216d4f4000a416b3b7be12d1c6
camila-frangp <camila.frangp@gmail.com>
2022-09-18 12:39:56 -0300

    draft campaigns

--- src/pages/Campaigns/Campaigns.tsx
+++ src/pages/Campaigns/Campaigns.tsx
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Input } from '@components/index';
+import CampaignsTable from './components/CampaignsTable/CampaignsTable';
+import TemplateCard from './components/TemplateCard/TemplateCard';
+import TemplateModal from './components/TemplateModal/TemplateModal';
+import { DiscoPaths } from '@routes/models/path.model';
+import { data as dataTemplate } from '@mocks/templateMatrix.mock';
+import './creativeMatrix.scss';
 
-const Campaigns = () => <div>Campaigns</div>;
+const Campaigns = () => {
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      {/* Modal detail: Template matrix selected */}
+
+        <TemplateModal
+          activeModal={open}
+          openModal={setOpen}
+        />
+
+      <section>
+        <h1>Creative Matrix</h1>
+        {/* Template section */}
+        <section>
+          <h2>Start with a Template</h2>
+          <TemplateCard
+            openModal={value => {
+              setOpen(true);
+            }}
+          />
+        </section>
+        {/* Creative matrix section */}
+        <section>
+          <div className="navTable">
+            <div>
+              <Input placeholder="Search" />
+              <Button>Search</Button>
+            </div>
+            <Button onClick={() => navigate(`/${DiscoPaths.NewCampaignsStep1}`)}>
+              New
+            </Button>
+          </div>
+          <CampaignsTable data={matrixList} />
+        </section>
+      </section>
+    </>
+  );
+};
 
 export default Campaigns;
--- src/routes/models/path.model.ts
+++ src/routes/models/path.model.ts
@@ -7,6 +7,8 @@ export enum DiscoPaths {
   Campaigns = 'campaigns',
   NewMatrixStep1 = 'creative-matrix/new-1',
   NewMatrixStep2 = 'creative-matrix/new-2',
+  NewCampaignsStep1 = 'campaigns/new-1',
+  NewCampaignsStep2 = 'campaigns/new-2',
 }
 
 export interface DiscoPages {
