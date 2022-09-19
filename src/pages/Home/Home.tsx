83c97f2c057977dae3e40b286e48afb235e2fb73
camila-frangp <camila.frangp@gmail.com>
2022-09-19 17:19:06 -0300

    base layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,36 @@
 import React, { useState } from 'react';
 import xaxisLogo from '../../assets/images/xaxis.svg';
 import { Button, Input, Select, Modal } from '@components/index';
+import Layout from '../common/Layout/Layout';
 import './home.scss';
 
 const Home = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div>
-      <img src={xaxisLogo} alt="logo" />
-      <Button onClick={() => setOpen(true)}>button</Button>
-      <Input value="input" />
-      <Select
-        options={[
-          { label: 'option A', value: 'option A' },
-          { label: 'option B', value: 'option B' },
-          { label: 'option C', value: 'option C' },
-        ]}
-      />
-      <Modal
-        className="exampleModal"
-        active={open}
-        onClose={() => setOpen(false)}>
-        <div>Modal example</div>
-        <div>
-          <Button onClick={() => setOpen(false)}>close</Button>
-        </div>
-      </Modal>
-    </div>
+    <Layout>
+      <div>
+        <img src={xaxisLogo} alt="logo" />
+        <Button onClick={() => setOpen(true)}>button</Button>
+        <Input value="input" />
+        <Select
+          options={[
+            { label: 'option A', value: 'option A' },
+            { label: 'option B', value: 'option B' },
+            { label: 'option C', value: 'option C' },
+          ]}
+        />
+        <Modal
+          className="exampleModal"
+          active={open}
+          onClose={() => setOpen(false)}>
+          <div>Modal example</div>
+          <div>
+            <Button onClick={() => setOpen(false)}>close</Button>
+          </div>
+        </Modal>
+      </div>
+    </Layout>
   );
 };
 
