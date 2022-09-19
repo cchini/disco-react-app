a2216e2f91f0b07f585fd53d29f8e60d79ba26f1
camila-frangp <camila.frangp@gmail.com>
2022-09-18 23:28:50 -0300

    modal campaigns

--- src/routes/DiscoPaths.ts
+++ src/routes/DiscoPaths.ts
@@ -2,8 +2,7 @@ import { DiscoPaths, DiscoPages } from './models/path.model';
 import Home from '../pages/Home/Home';
 import AccountSetup from '../pages/AccountSetup/IntegratedAccount';
 import Campaigns from '../pages/Campaigns/Campaigns';
-import NewCampaignsStep1 from '../pages/Campaigns/NewCampaigns/NewCampaignsStep1';
-import NewCampaignsStep2 from '../pages/Campaigns/NewCampaigns/NewCampaignsStep2';
+import NewCampaigns from '../pages/Campaigns/NewCampaigns/NewCampaigns';
 import CreativeMatrix from '../pages/CreativeMatrix/CreativeMatrix';
 import NewMatrixStep1 from '../pages/CreativeMatrix/NewMatrix/NewMatrixStep1';
 import NewMatrixStep2 from '../pages/CreativeMatrix/NewMatrix/NewMatrixStep2';
@@ -35,11 +34,7 @@ export const discoPaths: DiscoPages[] = [
     path: DiscoPaths.Campaigns,
   },
   {
-    component: NewCampaignsStep1,
-    path: DiscoPaths.NewCampaignsStep1,
-  },
-  {
-    component: NewCampaignsStep2,
-    path: DiscoPaths.NewCampaignsStep2,
+    component: NewCampaigns,
+    path: DiscoPaths.NewCampaigns,
   },
 ];
