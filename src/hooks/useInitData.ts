d70809fb38d8b60b2c7c7f1d2dec1a4de10273f8
giannina <giannecchini.a@gmail.com>
2022-09-20 15:33:57 -0300

    fix: init data api

---
+++ src/hooks/useInitData.ts
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { CreativeMatrix } from '@models/creativeMatrix.model';
+import useFetchAndLoad from '@hooks/useFetchAndLoad';
+import { getAccounts } from '@services/accountSetup.service';
+import { getAllCreativeMatrix } from '@services/creativeMatrix.service';
+import { useAsync } from '@hooks/useAsyncAxios';
+import { allCreativeMatrixAdapter } from '@adapters/creativeMatrix.adapter';
+import { AccountOption } from '@models/account.model';
+import { accountOptionAdapter } from '@adapters/account.adapter';
+
+const useInitData = (): [AccountOption[], CreativeMatrix[], boolean] => {
+  const [accountList, setAccounts] = useState<AccountOption[]>([]);
+  const [matrixList, setMatrixList] = useState<CreativeMatrix[]>([]);
+  const accounts = useFetchAndLoad();
+  const matrix = useFetchAndLoad();
+  const getApiAccounts = async () => await accounts.callEndpoint(getAccounts());
+  const getApiMatrix = async () =>
+    await matrix.callEndpoint(getAllCreativeMatrix());
+  useAsync(
+    getApiAccounts,
+    (response: any) => setAccounts(accountOptionAdapter(response)),
+    () => {},
+  );
+  useAsync(
+    getApiMatrix,
+    response => setMatrixList(allCreativeMatrixAdapter(response, true)),
+    () => {},
+  );
+
+  return [accountList, matrixList, accounts.loading && matrix.loading];
+};
+
+export default useInitData;
